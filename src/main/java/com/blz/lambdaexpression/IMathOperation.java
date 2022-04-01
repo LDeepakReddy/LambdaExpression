67c79409880215dacc6f2caec41f5033a0f320b5
LDeepakReddy <l.deepakreddy.com>
2022-04-01 22:21:05 +0530

    Added: feature to perform arithamatical operation by lambda expression

--- src/main/java/com/blz/lambdaexpression/IMathOperation.java
+++ src/main/java/com/blz/lambdaexpression/IMathOperation.java
@@ -1,4 +1,9 @@
 package com.blz.lambdaexpression;
 
+@FunctionalInterface
 
+public interface IMathOperation {
+    int calculate(int a, int b);
+
+}
 
--- src/main/java/com/blz/lambdaexpression/MathOperation.java
+++ src/main/java/com/blz/lambdaexpression/MathOperation.java
@@ -2,6 +2,16 @@ package com.blz.lambdaexpression;
 
 public class MathOperation {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Functional programming");
+
+        IMathOperation addition = (a, b) -> a + b;
+        IMathOperation subtraction = (a, b) -> a - b;
+        IMathOperation multiplication = (a, b) -> a * b;
+        IMathOperation division = (a, b) -> a / b;
+
+        System.out.println(addition.calculate(7, 7));
+        System.out.println(subtraction.calculate(5, 7));
+        System.out.println(multiplication.calculate(7, 7));
+        System.out.println(division.calculate(49, 8));
+
     }
 }
