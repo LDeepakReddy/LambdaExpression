521fe897ee80c2a625386bb4228e55758cb245f8
LDeepakReddy <l.deepakreddy.com>
2022-04-01 21:58:35 +0530

    Added welcome message to the program

---
+++ src/main/java/com/blz/lambdaexpression/MathOperation.java
@@ -0,0 +1,7 @@
+package com.blz.lambdaexpression;
+
+public class MathOperation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Functional programming");
+    }
+}
