5d94ff292da4826395cf4b9e335b138509818ef2
LDeepakReddy <l.deepakreddy.com>
2022-04-02 00:23:03 +0530

    Added feature to convert integer values into double values

--- src/main/java/com/blz/lambdaexpression/NumberPlayList.java
+++ src/main/java/com/blz/lambdaexpression/NumberPlayList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class NumberPlayList {
     public static void main(String[] args) {
@@ -11,6 +12,11 @@ public class NumberPlayList {
         list.add(7);
         list.add(4);
         list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(2);
+        list.add(3);
+        list.add(5);
 
         for (Integer numList : list) {
             System.out.println(numList);
@@ -46,6 +52,9 @@ public class NumberPlayList {
         //  Implicit lambda function
         System.out.println("traverse through Implicit lambda function");
         list.forEach(value -> System.out.println("Values in list " + value));
+
+        Function<Integer, Double> doubleFunction = (n) -> n.doubleValue();
+        list.forEach(item -> System.out.println(" Integer Value to Double : " + doubleFunction.apply(item)));
     }
 }
 
